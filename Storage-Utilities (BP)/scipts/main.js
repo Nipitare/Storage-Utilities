09fa340830da433732f51f99570eef8ab864d4f4
Nipitare <nipitareyt@gmail.com>
2024-10-29 19:55:57 +0200

    Changed mcstructrue files and updated main.js

--- Storage-Utilities (BP)/scipts/main.js
+++ Storage-Utilities (BP)/scipts/main.js
@@ -10,18 +10,12 @@ const StorageUtilities = new Rule({
     description: 'Disables or enables Storage Utilities',
 });
 extension.addRule(StorageUtilities)
-mc.world.afterEvents.buttonPush.subscribe((event) => {
-    if (!StorageUtilities.getValue()) 
-        return;
-    if (event.source === undefined)
-        return;
-    event.source.sendMessage('§aYou pushed a button!');
-});
+
 
 const ItemSetCommand = new Command({
-    name: 'itemset', // The name of the command
+    name: 'itemSet', // The name of the command
     description: 'Load structure file of a certain item set.', // Shows up in the help command. Can be a string or RawMessage type.
-    usage: 'itemset [message]', // The usage of the command that shows up in the help command & when used incorrectly
+    usage: 'itemSet [name]', // The usage of the command that shows up in the help command & when used incorrectly
     callback: exampleCommandCallback, // The function to run when the command is executed
     // Optional:
     args: [
@@ -30,7 +24,12 @@ const ItemSetCommand = new Command({
     contingentRules: ['StorageUtilities'], // Rules that must be true for the command to be enabled
     adminOnly: false, // Whether the command can only be run by admins (users with the 'CanopyAdmin' tag)
     helpEntries: [ // Additional help entries that show up in the help command
-       // { usage: `itemset`, description: `test xd` }, // Description can be a string or RawMessage type.
+     { usage: `itemSet casual`, description: `Loads casual item set` }, // Description can be a string or RawMessage type.
+     { usage: `itemSet TMC`, description: `Loads TMC item set` },
+     { usage: `itemSet mixed`, description: `Loads mixed item set` },
+     { usage: `itemSet split`, description: `Loads split item set` },
+     { usage: `itemSet nonstackable`, description: `Loads nonstackable item set` },
+     { usage: `itemSet bulk`, description: `Loads bulk item set` },
     ],
     helpHidden: false // Whether the command should be hidden from the help command.
 });
@@ -38,13 +37,14 @@ extension.addCommand(ItemSetCommand);
 
 function exampleCommandCallback(sender, args) {
     let { message } = args;
-    const player = mc.world.getPlayers()[0]; 
+    let player = mc.world.getPlayers()[0];
+    let {x, y, z} = player.location 
         message = message.toString();
-                if (message === 'test') {
-          mc.world.structureManager.place("mystructure:teststruc", player.dimension, player.location)
-          sender.sendMessage('§aLoaded item set Test') 
+                if (message === 'split') {
+          mc.world.structureManager.place("mystructure:split item set", player.dimension, {x: x+1 ,y: y, z: z+1} )
+          sender.sendMessage('§aLoaded item set split') 
         }
-        if (message !== 'test') {
+        if (message !== 'split') {
             sender.sendMessage('§cNot an valid item set please try again')
         }
 }
