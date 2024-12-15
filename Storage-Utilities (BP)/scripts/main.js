7befe52f4eda8ba7c7b52f87d86116980a143b96
Firebee <160149106+Firebee45@users.noreply.github.com>
2024-12-15 08:48:10 +0000

    Add files via upload
    Updated these to add itemsummon

--- Storage-Utilities (BP)/scripts/main.js
+++ Storage-Utilities (BP)/scripts/main.js
@@ -1,5 +1,12 @@
+//commands 
 import 'src/commands/SSCommand'
 import 'src/commands/UtilityItemsCommand'
 import 'src/commands/ItemSetCommand'
+import 'src/commands/ItemSummonCommand'
+
+//script events
+import 'src/scriptevents/ItemSummonCommand'
+
+//other
 import 'config'
 import 'rule'
